cb1f97fcb256e28d1b5fcace980392833c0ed5f3
Rushit <rushitparmar59@gmail.com>
2023-05-04 23:29:04 -0500

    Autocomplete finished

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,10 +3,8 @@
 
 const movieName = document.querySelector('.movie-name');
 const cityName = document.querySelector('.city-name');
-const movieList = document.querySelector('.movie-list nav ul');
-const cityList = document.querySelector('.city-list nav ul');
-
-
+var movieList = document.querySelector('.movie-list nav ul');
+var cityList = document.querySelector('.city-list nav ul');
 
 
 const url = './assets/script/movies.json';
@@ -51,3 +49,94 @@ async function getMovies() {
 window.addEventListener('load', () => {
   getMovies();
 })
+
+
+const movieListName = [
+  "Avengers: Infinity war", "Eternals", "The Devil Wears Prada", 
+  "Spider-Man: No Way Home", "The Flash", "The Lion King", "The Shining", 
+  "Toy Story 3", "No Time To Die", "The Batman", "Gone In 60 Seconds",
+  "F9: The Fast Saga"
+];
+
+
+/* Auto complete feature for movies */
+
+movieName.addEventListener('keyup', function() {
+  let value = movieName.value.toLowerCase().trim();
+  movieList.innerHTML = '';
+  movieList.style.display = 'none';
+  let count = 0;
+
+  // Show options of the movies that match
+  if (value.length > 1) {
+    movieListName.forEach(element => {
+      const name = element.toLowerCase();
+
+      if (name.includes(value)) {
+        movieList.innerHTML += `<li class="movie-element">${element}</li>`;
+        movieList.style.display = 'block';
+        count++;
+      }
+    })
+
+    // Movie not found 
+    if (count === 0) {
+      movieList.style.display = 'block';
+      movieList.innerHTML = `<li>Movie not found</li>`;
+    }
+
+    var movieOption = document.querySelectorAll('.movie-element');
+    for (let i = 0; i < movieOption.length; i++ ) {
+      movieOption[i].addEventListener('click', () => {
+        movieName.value = movieOption[i].innerText;
+        movieList.innerHTML = '';
+        movieList.style.display = 'none';
+      })
+    }
+    
+    console.log(movieOption);
+  }
+})
+
+
+const cityListName = [
+  'Alberta', 'Ottawa', 'Halifax', 'Quebec', 'Saskatchewan', 'Vancouver',
+  'Victoria', 'Winnipeg'
+];
+
+/* Auto complete feature for cities */
+cityName.addEventListener('keyup', function() {
+  let value = cityName.value.toLowerCase().trim();
+  cityList.innerHTML = '';
+  cityList.style.display = 'none';
+  let count = 0;
+
+  // Show options of the movies that match
+  if (value.length > 1) {
+    cityListName.forEach(element => {
+      const name = element.toLowerCase();
+
+      if (name.includes(value)) {
+        cityList.innerHTML += `<li class="city-element">${element}</li>`;
+        cityList.style.display = 'block';
+        count++;
+      }
+    })
+
+    // City not found 
+    if (count === 0) {
+      cityList.style.display = 'block';
+      cityList.innerHTML = `<li>City not found</li>`;
+    }    
+
+    var cityOption = document.querySelectorAll('.city-element');
+    for (let i = 0; i < cityOption.length; i++ ) {
+      cityOption[i].addEventListener('click', () => {
+        cityName.value = cityOption[i].innerText;
+        cityList.innerHTML = '';
+        cityList.style.display = 'none';
+      })
+    }    
+  }
+})
+
